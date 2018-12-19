ca893ceb3cfc2d80d3f5e0c7fc7c65d8c82a8130
jth2747 <45479809+jth2747@users.noreply.github.com>
2018-12-19 11:07:07 +0900

    Add files via upload

--- conway.c
+++ conway.c
@@ -1,39 +1,50 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <curses.h>
 
+
 int **board, **temp;
+
+typedef struct xy {
+	int x;
+	int y;
+} xy;
 
 void mkarr(int, int);
 void nextboard(int, int);
 int checkarr(int, int);
 void printarr(int, int);
+void title();
+void setboard()
+xy moveto(int, int, char);
 
 int main(int ac, char*av[]) {
 
 	int row, col, i, j, k;
 
+	title();
+	setting();
+
+
 	row = atoi(av[1]);
 	col = atoi(av[2]);
 
 	mkarr(row, col);
-	board[6][6] = 1;
-	board[7][7] = 1;
-	board[5][5] = 1;
-	board[6][5] = 1;
+
+	setboard();
 
 	initscr();
 	clear();
-	for (k = 0; k < 10; k++) {
+	for (k = 0; k < turn; k++) {
 
-		for (i = 1; i < row; i++) {
-			for (j = 1; j < col; j++) {
+		for (i = 0; i < row; i++) {
+			for (j = 0; j < col; j++) {
 				move(i, j);
 				standout();
 
-				if (board[i][j] == 0) {
+				if (board[i + 1][j + 1] == 0) {
 
 					if (has_colors() == FALSE)
 					{
@@ -44,21 +55,23 @@ int main(int ac, char*av[]) {
 					start_color();         /* Start color          */
 					init_pair(1, COLOR_RED, COLOR_BLACK);
 
-					attron(COLOR_PAIR(1));
+					attron(COLOR_PAIR(2));
 					addstr(" ");
-					attroff(COLOR_PAIR(1));
+					attroff(COLOR_PAIR(2));
 
 				}
-				else addstr(" ");
+				else {
+					addstr(" ");
+				}
 				standend();
 			}
 
 		}
 		refresh();
-		sleep(1);
-		move(1, 1);
-		for (i = 1; i < row; i++)
-			for (j = 1; j < col; j++)addstr(" ");
+		usleep(speed);
+		move(0, 0);
+		for (i = 0; i < row; i++)
+			for (j = 0; j < col; j++)addstr(" ");
 		move(1, 1);
 		nextboard(row, col);
 	}
@@ -89,14 +102,14 @@ void nextboard(int row, int col) {
 
 			switch (checkarr(i, j)) {
 
-			case 2://»óÅÂ À¯Áö
+			case 2://상태 유지
 				break;
 
-			case 3://»ì¾Æ³²
+			case 3://살아남
 				temp[i][j] = 1;
 				break;
 
-			default://Á×´Â´Ù
+			default://죽는다
 				temp[i][j] = 0;
 				break;
 
@@ -151,6 +164,7 @@ void printarr(int row, int col) {
 
 
 }
+
 void title() {//메인화면 출력
 	int i, j, row = 24, col = 80;
 	char arr[24][80] = { "********************************************************************************",
@@ -185,7 +199,7 @@ void title() {//메인화면 출력
 		for (j = 0; j < col; j++) {
 			move(i, j);
 			standout();
-			start_color();			/* Start color */
+			start_color();         /* Start color */
 			init_pair(1, COLOR_BLACK, COLOR_WHITE);
 
 			attron(COLOR_PAIR(1));
@@ -199,3 +213,136 @@ void title() {//메인화면 출력
 	endwin();
 
 }
+
+void setboard() {
+	int i, j, row = LINES, col = COLS;
+	char ch;
+	xy result;
+
+	initscr();
+	clear();
+	keypad(stdscr, TRUE);
+	noecho();
+
+	//print message
+	move(9, 27);
+	addstr("Press Space To Set Board!");
+	move(10, 27);
+	addstr("Press Enter To Start Game");
+	refresh();
+	sleep(3);
+	move(9, 27);
+	addstr("                         ");
+	move(10, 27);
+	addstr("                         ");
+	refresh();
+	//init board
+	for (i = 0; i < row; i++) {
+		for (j = 0; j < col; j++) {
+			move(i, j);
+			addch(' ');
+		}
+	}
+
+	//start set board
+	ch = '\0';
+	move(0, 0);
+	i = 0;
+	j = 0;
+
+	if (has_colors() == FALSE)
+	{
+		endwin();
+		printf("Your terminal does not support color\n");
+		exit(1);
+	}
+
+
+	start_color();			/* Start color 			*/
+	init_pair(1, COLOR_WHITE, COLOR_BLACK);
+	init_pair(2, COLOR_BLACK, COLOR_WHITE);
+	while (1) {
+
+		ch = getch();
+		if (ch == '\n')
+			break;
+
+		if (ch == ' ') {
+
+			if (board[i + 1][j + 1] == 1) {
+				board[i + 1][j + 1] = 0;
+
+				standout();
+				attron(COLOR_PAIR(2));
+				addch(' ');
+				attroff(COLOR_PAIR(2));
+				standend();
+
+			}
+
+			else {
+
+				standout();
+
+
+				attron(COLOR_PAIR(1));
+				addch(' ');
+				attroff(COLOR_PAIR(1));
+				standend();
+				board[i + 1][j + 1] = 1;
+			}
+
+		}
+
+		else {
+			result = moveto(i, j, ch);
+
+			move(result.x, result.y);
+			i = result.x;
+			j = result.y;
+		}
+		refresh();
+	}
+
+	endwin();
+
+}
+
+xy moveto(int x, int y, char key) {
+	xy result;
+	switch (key) {
+
+	case 'w':
+		x--;
+		break;
+
+	case 's':
+		x++;
+		break;
+
+	case 'a':
+		y--;
+		break;
+
+	case 'd':
+		y++;
+		break;
+
+	}
+	if (x < 0) {
+		x = LINES - 1;
+	}
+	else if (x > LINES - 1) {
+		x = 0;
+	}
+
+	if (y > COLS - 1)
+		y = 0;
+	else if (y < 0)
+		y = COLS - 1;
+
+	result.x = x;
+	result.y = y;
+
+	return result;
+}}
